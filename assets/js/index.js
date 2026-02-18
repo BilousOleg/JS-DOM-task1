c8190d090915767e0b4dbdc9e85525fb3ddac8b7
BilousOleg <olezhka82802@gmail.com>
2026-02-18 16:06:16 +0200

    Minor changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@
 
 const newSectionEl = document.createElement('section');
 
+// Тут можна додати перевірку при створенні id, але я думаю, що робити елемент рутовим двічі ніхто не буде
 newSectionEl.setAttribute('id', 'root');
 newSectionEl.textContent = 'Lorem ipsum';
 newSectionEl.style.color = 'blue';
